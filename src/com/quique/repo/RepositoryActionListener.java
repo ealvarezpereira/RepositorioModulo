db4471f2c4514aebe2e4fb336e064dff41aa05ce
ealvarezpereira@danielcastelao.org <quique@quique-Aspire-E5-575G>
2018-04-20 13:16:53 +0200

    Modificado codigo para correcto funcionamiento

--- src/com/quique/repo/RepositoryActionListener.java
+++ src/com/quique/repo/RepositoryActionListener.java
@@ -7,15 +7,17 @@ package com.quique.repo;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+import org.kohsuke.github.GHCreateRepositoryBuilder;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
-import org.kohsuke.github.*;
+import org.kohsuke.github.GitHub;
 
 //ActionID que a un fichero le asigna un ID de la clase
-
 @ActionID(
         category = "File",
         id = "com.quique.repo.RepositoryActionListener"
@@ -35,14 +37,30 @@ import org.kohsuke.github.*;
 
 /**
  * @param git es una variable de tipo GitHub para crear el repositorio
+ * @param usu es el usuario de GitHub
+ * @param ctra es la contraseña de GitHub
  */
 public final class RepositoryActionListener implements ActionListener {
-    
-    static GitHub git;
-    
+
+    static GitHub github;
+
     @Override
     public void actionPerformed(ActionEvent e) {
-        
-        
+        try {
+            //Pedimos el usuario y la contraseña para poder conectarnos a github
+            String usu = JOptionPane.showInputDialog("Usuario");
+            String ctra = JOptionPane.showInputDialog("Contraseña");
+
+            //Nos conectamos a github utilizando el usuario y la contraseña pasandoselos por parametos
+            github = GitHub.connectUsingPassword(usu, ctra);
+            //Pedimos el nombre del repositorio
+            String repoNombre = JOptionPane.showInputDialog("Introduzca el nombre del repositorio");
+
+            //Creamos un objeto GHCreateRepositoryBuilder y le asignamos la creación del repositorio
+            GHCreateRepositoryBuilder repo = github.createRepository(repoNombre);
+            repo.create();
+        } catch (IOException ex) {
+            System.out.println("Error repositorio. " + ex);
+        }
     }
 }
